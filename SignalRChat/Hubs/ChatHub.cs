dccfca2d96dc0a19bd88f41a481725999304dc0d
张经纬 <zjw0621@ly.com>
2018-07-19 13:58:35 +0800

    new

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Habs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessagesToPage(name, message);
+        }
+    }
+}
